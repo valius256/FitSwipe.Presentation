113c4b3145db30250577f7cce082d9f788e4d4e7
tinbusiness-work <tinbusiness.work@gmail.com>
2024-09-19 10:44:57 +0700

    Still Working

---
+++ FitSwipe.Mobile/Pages/PayingPages/PayingCheck.xaml.cs
@@ -0,0 +1,9 @@
+namespace FitSwipe.Mobile.Pages.PayingPages;
+
+public partial class PayingCheck : ContentPage
+{
+	public PayingCheck()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FitSwipe.Mobile/Pages/PayingPages/PayingView.xaml.cs
@@ -0,0 +1,9 @@
+namespace FitSwipe.Mobile.Pages.PayingPages;
+
+public partial class PayingView : ContentPage
+{
+	public PayingView()
+	{
+		InitializeComponent();
+	}
+}
