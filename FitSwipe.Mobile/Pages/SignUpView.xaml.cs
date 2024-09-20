c74c68b373bc33f51561c5aa913ac194c51cd2d3
Stayer1231 <phongntse171371@fpt.edu.vn>
2024-09-20 15:00:21 +0700

    features: allowing user to switch between PT or trainee signup with seperated render UI

--- FitSwipe.Mobile/Pages/SignUpView.xaml.cs
+++ FitSwipe.Mobile/Pages/SignUpView.xaml.cs
@@ -1,12 +1,29 @@
-using FitSwipe.Mobile.ViewModels;
+﻿using FitSwipe.Mobile.ViewModels;
+using System.ComponentModel;
 
 namespace FitSwipe.Mobile.Pages;
 
 public partial class SignUpView : ContentPage
 {
-    public SignUpView(SignUpViewModel viewModel)
+    public SignUpView (SignUpViewModel viewModel)
     {
         InitializeComponent();
-        BindingContext = viewModel;
+        BindingContext = viewModel; // Bind to ViewModel
+    }
+
+    private void ptRadioButton_CheckedChanged (object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            ((SignUpViewModel)BindingContext).SelectedRole = "pt";
+        }
+    }
+
+    private void traineeRadioButton_CheckedChanged (object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            ((SignUpViewModel)BindingContext).SelectedRole = "trainee";
+        }
     }
 }
