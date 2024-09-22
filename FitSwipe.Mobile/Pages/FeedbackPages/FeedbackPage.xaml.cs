8bfd7f7436d7705108b57e2a87edc9eaebd3adbe
tinbusiness-work <tinbusiness.work@gmail.com>
2024-09-22 14:46:27 +0700

    Missing Star + Image add *Add 5 png in Icons

--- FitSwipe.Mobile/Pages/FeedbackPages/FeedbackPage.xaml.cs
+++ FitSwipe.Mobile/Pages/FeedbackPages/FeedbackPage.xaml.cs
@@ -1,9 +1,45 @@
+using System.Collections.ObjectModel;
+
 namespace FitSwipe.Mobile.Pages.FeedbackPages;
 
-public partial class NewPage1 : ContentPage
+public partial class FeeedbackPage : ContentPage
 {
-	public NewPage1()
+
+    // Declare ImageItems as a class-level property
+    public ObservableCollection<ImageItem> ImageItems { get; set; }
+
+    public FeeedbackPage()
 	{
 		InitializeComponent();
-	}
+
+        // Initialize the ImageItems collection
+        ImageItems = new ObservableCollection<ImageItem>();
+
+        // Bind the ImageItems collection to the CollectionView
+        imageCollectionView.ItemsSource = ImageItems;
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var result = await MediaPicker.PickPhotoAsync();
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+                var imageSource = ImageSource.FromStream(() => stream);
+
+                // Add the new image to the collection
+                ImageItems.Add(new ImageItem { ImageSource = imageSource });
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions
+        }
+    }
+}
+public class ImageItem
+{
+    public ImageSource ImageSource { get; set; }
 }
