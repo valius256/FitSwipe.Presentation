b5ee5734252540d2f5b4c59af85c5fb2957e4f0a
tinbusiness-work <tinbusiness.work@gmail.com>
2024-09-26 22:04:49 +0700

    Done 90%, need fix png logo

---
+++ FitSwipe.Mobile/Pages/UploadAvatar/TraineeUploadAavatar.xaml.cs
@@ -0,0 +1,9 @@
+namespace FitSwipe.Mobile.Pages.UploadAvatar;
+
+public partial class TraineeUploadAavatar : ContentPage
+{
+	public TraineeUploadAavatar()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FitSwipe.Mobile/Pages/UploadAvatar/UserUploadAvatar.xaml.cs
@@ -0,0 +1,83 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FitSwipe.Mobile.Pages.UploadAvatar;
+
+public partial class UserUploadAvatar : ContentPage
+{
+    private bool _isPhotoCaptured;
+    private ImageSource _capturedImageSource;
+
+    public UserUploadAvatar()
+	{
+		InitializeComponent();
+        BindingContext = this;
+    }
+    public bool IsPhotoCaptured
+    {
+        get => _isPhotoCaptured;
+        set
+        {
+            _isPhotoCaptured = value;
+            OnPropertyChanged();
+        }
+    }
+    public ImageSource CapturedImageSource
+    {
+        get => _capturedImageSource;
+        set
+        {
+            _capturedImageSource = value;
+            OnPropertyChanged();
+        }
+    }
+    private async void cameraPhotoButton_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+                if (photo != null)
+                {
+                    // Load the captured image into the Image control
+                    var stream = await photo.OpenReadAsync();
+                    CapturedImageSource = ImageSource.FromStream(() => stream);
+
+                    // Hide the buttons and display the photo
+                    IsPhotoCaptured = true;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
+    private async void galleryPhotoButton_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var result = await MediaPicker.Default.PickPhotoAsync();
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+                CapturedImageSource = ImageSource.FromStream(() => stream);
+                IsPhotoCaptured = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
+    // Notify property changed for data binding
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
