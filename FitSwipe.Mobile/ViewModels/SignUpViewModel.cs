961ef5c9aafcb4dc505c0caf866aac341a1e430f
kisphapha <thanhhung16082003@gmail.com>
2024-09-29 21:31:57 +0700

    fetch register

--- FitSwipe.Mobile/ViewModels/SignUpViewModel.cs
+++ FitSwipe.Mobile/ViewModels/SignUpViewModel.cs
@@ -1,12 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Firebase.Auth;
+using FitSwipe.Mobile.Controls;
+using FitSwipe.Shared.Dtos.Auth;
+using FitSwipe.Shared.Enums;
+using FitSwipe.Shared.Utils;
 
 namespace FitSwipe.Mobile.ViewModels
 {
     public partial class SignUpViewModel : ObservableObject
     {
         private readonly FirebaseAuthClient _authClient;
+        public LoadingDialog LoadingDialog { get; set; } = new LoadingDialog();
+
 
         public SignUpViewModel(FirebaseAuthClient authClient)
         {
@@ -22,21 +28,47 @@ namespace FitSwipe.Mobile.ViewModels
         [ObservableProperty] private string _password;
         [ObservableProperty] private string _confirmPassword;
         [ObservableProperty] private string selectedRole;
+        [ObservableProperty] private bool isAgreedWithTos = false;
 
         [RelayCommand]
         private async Task SignUp()
         {
             try
             {
-
-                await _authClient.CreateUserWithEmailAndPasswordAsync(Email, Password, Username);
-                await Application.Current.MainPage.DisplayAlert("Thành Công", $"Bạn {Email} đã đăng ký thành công ", "OK");
+                if ((_firstName == null && _lastName == null) || _email == null || _phoneNumber == null || _password == null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Thiếu thông tin", $"Vui lòng điền đầy đủ thông tin", "OK");
+                }
+                else if (_password != _confirmPassword)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Sai thông tin", $"Mật khẩu không khớp", "OK");
+                } else if (!isAgreedWithTos)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Điều khoản dịch vụ", $"Hãy đồng ý với điều khoản dịch vụ của chúng tôi để đăng ký", "OK");
+                }
+                else
+                {
+                    LoadingDialog.IsVisible = true;
+                    await Fetcher.PostAsync("api/Authentication/register", new RegisterRequestDto
+                    {
+                        Email = _email,
+                        Password = _password,
+                        Name = _firstName + " " + _lastName,
+                        Phone = _phoneNumber,
+                        Role = selectedRole == "trainee" ? Role.Trainee : Role.PT
+                    });
+                    await Shell.Current.GoToAsync($"//RegisterSuccessfully?role={selectedRole}");
+                }
+                
+                //await _authClient.CreateUserWithEmailAndPasswordAsync(Email, Password, Username);
+                //await Application.Current.MainPage.DisplayAlert("Thành Công", $"Bạn {Email} đã đăng ký thành công ", "OK");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                await Application.Current.MainPage.DisplayAlert("Lỗi", $"", "OK");
+                await Application.Current.MainPage.DisplayAlert("Lỗi", $"{ex.Message}", "OK");
 
             }
+            LoadingDialog.IsVisible = false;
         }
 
         [RelayCommand]
