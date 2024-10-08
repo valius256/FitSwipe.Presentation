9a752c1967bb4bf3876049172ab65512dd649e66
tinbusiness-work <tinbusiness.work@gmail.com>
2024-10-08 23:08:22 +0700

    UI PT , Trainee

---
+++ FitSwipe.Mobile/Pages/ProfilePages/PTReport.xaml.cs
@@ -0,0 +1,14 @@
+namespace FitSwipe.Mobile.Pages.ProfilePages;
+
+public partial class PTReport : ContentPage
+{
+	public PTReport()
+	{
+		InitializeComponent();
+	}
+
+    private async void sendReport_Clicked(object sender, EventArgs e)
+    {
+        //await Navigation.PushAsync(new TraineeUploadAvatar());
+    }
+}
---
+++ FitSwipe.Mobile/Pages/ProfilePages/TraineeReport.xaml.cs
@@ -0,0 +1,14 @@
+namespace FitSwipe.Mobile.Pages.ProfilePages;
+
+public partial class TraineeReport : ContentPage
+{
+	public TraineeReport()
+	{
+		InitializeComponent();
+	}
+
+    private async void sendReport_Clicked(object sender, EventArgs e)
+    {
+        //await Navigation.PushAsync(new TraineeUploadAvatar());
+    }
+}
