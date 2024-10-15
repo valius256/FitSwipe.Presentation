ae012a6c93aba46f67db73f61b014be5b173bf12
Stayer2109 <nthanhphong2109@gmail.com>
2024-10-15 14:56:25 +0700

    merge

---
+++ FitSwipe.Mobile/Pages/SubscriptionPages/SubscriptionResultView.xaml.cs
@@ -0,0 +1,9 @@
+namespace FitSwipe.Mobile.Pages.SubscriptionPages;
+
+public partial class SubscriptionResultView : ContentPage
+{
+	public SubscriptionResultView()
+	{
+		InitializeComponent();
+	}
+}
